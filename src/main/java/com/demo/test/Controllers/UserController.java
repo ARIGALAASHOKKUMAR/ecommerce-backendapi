02abaa4761e73da4f1934b0f3c5e0d864ca7a175
ARIGALA ASHOK KUMAR <ashok.arigala@apcfss.in>
2025-06-26 16:29:48 +0530

    added dummy api for cron job

--- src/main/java/com/demo/test/Controllers/UserController.java
+++ src/main/java/com/demo/test/Controllers/UserController.java
@@ -10,10 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -114,5 +111,10 @@ public class UserController {
             response.put("status", "error");
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
         }
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<?> test(@RequestBody UserEntity loginRequest){
+        return ResponseEntity.ok("hii");
     }
 }
